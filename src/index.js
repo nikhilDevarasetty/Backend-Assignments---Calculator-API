daf6f0fe105a19df721ef39c1da8bad801714adb
nikhilDevarasetty <nicholus158@gmail.com>
2020-12-01 23:12:26 +0530

    testing4.2

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ app.post("/divide", (req, res) => {
   if (typeof num1 === "string" || typeof num2 === "string") {
     res.status(500).send({ message: "Invalid data types" });
   } else if (num2 === 0) {
-    res.status(500).send({ message: "Cannot divide by zero" });
+    res.status(404).send({ message: "Cannot divide by zero" });
   } else if (num1 < -threshold || num2 < -threshold || result < -threshold) {
     res.status(500).send({ message: "Underflow" });
   } else if (num1 > threshold || num2 > threshold || result > threshold) {
